aafbf6e79bc1c5b82b87ce16206eef5133e42e1e
ignatiusdariel <ignatiusdariel@gmail.com>
2022-03-29 23:13:10 +0700

    Nambah comment untuk memperjelas

--- controllers/GoRoutine.go
+++ controllers/GoRoutine.go
@@ -5,25 +5,29 @@ import (
 	"time"
 )
 
-func compute(value int) {
-	for i := 0; i < value; i++ {
+// Go Routine merupakan function dan method yang berjalan berasamaan dengan function dan method yang lain
+// Cost membuat sebuah goroutine jauh lebih ringan dibandingkan dengan membuat sebuah thread
+func compute(value int, start int) {
+	for i := start; i < value; i++ {
 		time.Sleep(time.Second)
 		fmt.Println(i)
 	}
 }
 
+//Contoh funtion yang menggunakan go routine
 func GoRoutine() {
-	fmt.Println("Concurrency with GoRoutines")
+	fmt.Println("Concurrency WITH GoRoutines")
 
-	go compute(5)
-	go compute(5)
+	go compute(5, 1)
+	go compute(6, 2)
 
 	fmt.Scanln()
 }
 
+// Contoh function yang tidak menggunakan go routine
 func WithoutGoRoutine() {
-	fmt.Println("Concurrency with GoRoutines")
+	fmt.Println("Concurrency WITHOUT GoRoutines")
 
-	compute(5)
-	compute(5)
+	compute(5, 1)
+	compute(6, 2)
 }
