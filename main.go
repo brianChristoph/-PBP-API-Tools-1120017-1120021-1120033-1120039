f0d0c9a1b5c4ada60ce6753538a2993a40a87543
Matthew <matthewhariady@gmail.com>
2022-03-30 00:32:46 +0700

    masukin cons ke WithoutGoRoutine

--- main.go
+++ main.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	cons "PBP-API-Tools-1120017-1120021-1120033-1120039/controllers"
+	// "time"
 )
 
 func main() {
-	// cons.GoRoutine()
-	// WithoutGoRoutine()
+	cons.GoRoutine()
+	// cons.WithoutGoRoutine()
+	// time.Sleep(time.Minute)
 	// cons.GoMail()
 	// cons.CronConcept1()
 	// CronConcept2()
-	cons.RedisMain()
+	// cons.RedisMain()
 }
